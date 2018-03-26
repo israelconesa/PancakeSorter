02b00a76110203aee82a22939ac1edc26eaed674
ILerma <Israel.Lerma@aquilaheywood.co.uk>
2018-03-26 16:58:08 +0100

    Almost there

--- src/com/spaniard/PancakeStack.java
+++ src/com/spaniard/PancakeStack.java
@@ -13,7 +13,7 @@ public class PancakeStack {
     private ArrayList<String> sortedStack;
     private ArrayList<String> flippedStack;
     private ArrayList<String> subStack;
-    private ArrayList<String> indexOfBiggestPancake;
+    private ArrayList<String> interimStack;
     private List<Object> stackToFlip;
 
 
@@ -22,12 +22,11 @@ public class PancakeStack {
     public PancakeStack(){
         getOriginalStack();
         sortStack();
-        //reverseStack();
     }
 
-    // Read the second line of text file to gather the original pancake stack and returning to the constructor
-    public ArrayList<String> getOriginalStack(){
-        File textFile = new File("/home/israelconesa/Development/PancakeSorter/src/myfile.txt");
+    // Read the second line of text file to gather the original pancake stack and return it to the constructor
+    private ArrayList<String> getOriginalStack(){
+        File textFile = new File("/home/ilerma/Projects/PancakeSorter/src/myfile.txt");
         try {
             String line2 = Files.readAllLines(Paths.get(String.valueOf(textFile))).get(1);
             originalStack = new ArrayList<String>(Arrays.asList(line2.split(" ")));
@@ -37,55 +36,86 @@ public class PancakeStack {
         return this.originalStack;
     }
 
-    // Sort arraylist for solution
-    public ArrayList<String> sortStack(){
+    // Sort stack to learn the solution
+    private ArrayList<String> sortStack(){
         sortedStack = new ArrayList<>(originalStack);
         Collections.sort(sortedStack);
         return this.sortedStack;
     }
 
     // Flip any stack
-    public ArrayList<String> flipStack(ArrayList<String> stackToFlip) {
+    private ArrayList<String> flipStack(ArrayList<String> stackToFlip) {
         flippedStack = new ArrayList<>(stackToFlip);
         Collections.reverse(flippedStack);
         return this.flippedStack;
     }
 
     // Compare a stack with original stack for equality
-    public boolean areStacksEqual(ArrayList<String> listToCompare) {
+    private boolean areStacksEqual(ArrayList<String> listToCompare) {
         return listToCompare.equals(originalStack);
     }
 
-    // Creates substack from index[0] to index[max], where max is the highest
-    // value on said list and flips it
-    public ArrayList<String> subStackToLargest(ArrayList<String> subStackToFlip) {
-        String biggestPancake = Collections.max(subStackToFlip); // find largest pancake on stack
-        int sizeOfSubStack = subStackToFlip.size(); // find size of stack
-        int indexOfBiggestPancake = subStackToFlip.indexOf(biggestPancake); //find index of largest pancake on stack
-        if (indexOfBiggestPancake !=  sizeOfSubStack) {
-            ArrayList<String> subStack = new ArrayList<String>(subStackToFlip.subList(0, indexOfBiggestPancake + 1));
-            flipStack(subStack);
-        }else {
-            ArrayList<String> subStack = new ArrayList<String>(subStackToFlip.subList(0, indexOfBiggestPancake));
-            flipStack(subStack);
-        }
-        return this.subStack;
-    }
+    public void letTheFlippingBegin() {
 
-    // Compare arrays to get solution
+        interimStack = new ArrayList<>(getOriginalStack());
+
+        //String biggestPancake = Collections.max(interimStack); // find largest pancake on stack
+
+        //int sizeOfSubStack = interimStack.size(); // find size of stack
+
+        //int indexOfBiggestPancake = interimStack.indexOf(biggestPancake); //find index of largest pancake on stack
+
+        // Work your way from the bottom to the top of the stack
+        for (int i = interimStack.size()-1; i>=0; i--) {
+
+            System.out.println("Current Index value : " +i);
+
+            System.out.println("interim stack is now " +interimStack);
+
+            if (!sortedStack.get(i).equals(interimStack.get(i))) {
+
+                //identify a substack starting from the top and ending on the largest pancake
+                subStack = new ArrayList<String>(interimStack.subList(0, originalStack.indexOf(Collections.max(interimStack))+1));
+
+//                System.out.println("substack before flipping "+subStack);
+
+                //flip the substack
+                subStack = flipStack(subStack); // bring the biggest pancake to the top
+
+//                System.out.println("substack after flipping "+flippedStack);
+
+                //update the interim stack with the values of substack after they've been flipped
+                for (int j = 0; j<subStack.size(); j++) {
+
+                    interimStack.set(j, subStack.get(j));
+                }
+
+                // bring the biggest pancake to the bottom
+                interimStack = flipStack(interimStack);
+                System.out.println(interimStack + "------");
+
+            }else {
+
+
+
+                //flip the whole interim stack
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
 
         PancakeStack pancakeStack = new PancakeStack();
 
 
-        System.out.println("The original stack is : " + pancakeStack.originalStack);
+//        System.out.println("The original stack is : " + pancakeStack.originalStack);
+//
+//        System.out.println("The sorted stack is : " + pancakeStack.sortedStack);
+//
+//        System.out.println("Flipping the original stack : " + pancakeStack.flipStack(pancakeStack.originalStack));
 
-        System.out.println("The sorted stack is : " + pancakeStack.sortedStack);
-
-        System.out.println("Flipping the original stack : " + pancakeStack.flipStack(pancakeStack.originalStack));
-
-        System.out.println(pancakeStack.subStackToLargest(pancakeStack.originalStack));
+        pancakeStack.letTheFlippingBegin();
 
 
     }
