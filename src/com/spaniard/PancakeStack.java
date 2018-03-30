3662f65d7321199ac2bee5bd24eda416d92c8537
israelconesa <israelconesa@gmail.com>
2018-03-30 17:54:36 +0100

    Another promising solution

--- src/com/spaniard/PancakeStack.java
+++ src/com/spaniard/PancakeStack.java
@@ -27,7 +27,7 @@ public class PancakeStack {
      * I had cast the arraylist as the numbers, i.e. pancake sizes, could be two digits and
      * the sorting when strings would not provide the required results */
     private ArrayList<Integer> getOriginalStackNumbers() throws Exception {
-        File textFile = new File("/home/ilerma/Projects/PancakeSorter/src/myfile.txt");
+        File textFile = new File("/home/israelconesa/Development/PancakeSorter/src/myfile.txt");
         try {
             String line2 = Files.readAllLines(Paths.get(String.valueOf(textFile))).get(1);
             originalStack = new ArrayList<>(Arrays.asList(line2.split(" ")));
@@ -55,87 +55,85 @@ public class PancakeStack {
 
         int flipCounter = 0;
 
-        /* Work your way from the bottom to the top of the stack */
-        for (int i = interimStack.size()-1; i>=0; i--) {
-            System.out.println("----------------------------------------------");
+        /* The interim stack begins as a copy of the original and is updated as pancakes are rearranged by size.
+         * Its size drives the number of iterations through the stack starting from the bottom.*/
+        for (int i = interimStack.size() - 1; i >= 0; i--) {
+
             System.out.println("INDEX IS NOW : " + i);
-            System.out.println( !interimStack.get(i).equals(sortedStackNumbers.get(i)) && !interimStack.get(0).equals(sortedStackNumbers.get(i)));
 
-            if (
-                !interimStack.get(i).equals(sortedStackNumbers.get(i)) && //pancake under test is NOT at the bottom of the interim stack
-                !interimStack.get(0).equals(sortedStackNumbers.get(i))    //AND NOT at the top already
-                ) {
+            /* Pancake under test is NOT at the bottom of the interim stack.
+             * If it is, do nothing and start a new iteration */
+            if (!interimStack.get(i).equals(sortedStackNumbers.get(i))) {
 
+                /* Pancake under test is NOT at the TOP of the interim stack.
+                 * If it is, flip it internally to bring it to the bottom */
+                if (!interimStack.get(0).equals(sortedStackNumbers.get(i))) {
 
-                /* Identify a substack starting from the top and ending on the largest pancake.
-                 * Largest is identifiable by its position on the sorted stack */
-                subStack = new ArrayList<>(interimStack.subList(0, interimStack.lastIndexOf(sortedStackNumbers.get(i)) + 1));
-
-                /* Create another substack from that point to the bottom of the pile */
-                subStack2 = new ArrayList<>(interimStack.subList(interimStack.lastIndexOf(sortedStackNumbers.get(i)) + 1, interimStack.size()));
-
-                System.out.println(subStack);
-                System.out.println(subStack2);
-
-                /* Flip the subStack to bring the largest pancake to the top */
-                Collections.reverse(subStack);
-
-                /* Put together the stacks */
-                subStack.addAll(subStack2);
-
-                /* This is how the whole stack looks like now after bringing the largest pancake AT THE TOP */
-                interimStack = new ArrayList<>(subStack);
-
-                flipCounter = flipCounter + 1;
-
-                System.out.println("Flip " + flipCounter + " is " + interimStack);
-
-                /* Break the loop if the right sorting has been achieved
-                * by comparing with the solution. I do it here as I want to break
-                * as early as possible to make it faster... and carrying on
-                * would break the sorting :)*/
-                if (sortedStackNumbers.equals(interimStack)) break;
-
-                /* Ensure that pancake under test is NOT at the top */
-//                if (interimStack.get(0).equals(sortedStackNumbers.get(i))) {
-
-                    /* Identify a substack starting from the top and ending on the largest pancake */
-                    subStack = new ArrayList<>(interimStack.subList(0, i + 1));
-
-                    /* Create another substack from that point to the bottom of the pile */
-                    subStack2 = new ArrayList<>(interimStack.subList(i + 1, interimStack.size()));
-
-                    System.out.println(subStack);
-                    System.out.println(subStack2);
-
+                    /* Identify a substack starting from the top and ending on the largest pancake.
+                     * Largest is identifiable by its position on the sorted stack */
+                    subStack = new ArrayList<>(interimStack.subList(0, (interimStack.lastIndexOf(sortedStackNumbers.get(i)) + 1)));
                     Collections.reverse(subStack);
 
-                    /* Put together the stacks */
-                    subStack.addAll(subStack2);
+                    System.out.println(subStack);
 
-                    /* This is how the whole stack looks like now after bringing the largest pancake AT THE BOTTOM */
-                    interimStack = new ArrayList<>(subStack);
+                    for (int j = 0; j <= interimStack.lastIndexOf(sortedStackNumbers.get(i)); j++) {
+
+                        interimStack.set(j, subStack.get(j));
+                    }
 
                     flipCounter = flipCounter + 1;
 
+                    /* This is how the whole stack looks like now after bringing the largest pancake AT THE TOP */
                     System.out.println("Flip " + flipCounter + " is -----" + interimStack);
+
+                    /* Break the loop if the right sorting has been achieved
+                    * by comparing with the solution. I do it here as I want to break
+                    * as early as possible to make it faster... and carrying on
+                    * would break the sorting :)*/
+                    if (sortedStackNumbers.equals(interimStack)) break;
+
+                    subStack2 = new ArrayList<>(interimStack.subList(0, i + 1));
+                    Collections.reverse(subStack2);
+
+                    for (int j = 0; j <= i ; j++) {
+
+                        interimStack.set(j, subStack2.get(j));
+
+                    }
+
+                    flipCounter = flipCounter + 1;
+
+                    /* This is how the whole stack looks like now after bringing the largest pancake AT THE BOTTOM */
+                    System.out.println("Flip " + flipCounter + " is -----" + interimStack);
+
+                    System.out.println("@@@@@@@@@@@@@@");
+
+                    if (sortedStackNumbers.equals(interimStack)) break;
+
+                } else {
+
+                    subStack2 = new ArrayList<>(interimStack.subList(0, i + 1));
+                    Collections.reverse(subStack2);
+
+                    for (int j = 0; j <= i ; j++) {
+
+                        interimStack.set(j, subStack2.get(j));
+
+                    }
+
+                    flipCounter = flipCounter + 1;
+                    System.out.println("Flip " + flipCounter + " is -----" + interimStack);
+
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~");
 
                     /* Break the loop if the right sorting has been achieved
                     * by comparing with the solution */
                     if (sortedStackNumbers.equals(interimStack)) break;
-
-//                }else {
-//
-//                }
-
-            } else {
-                /* Pancake under test is already at the bottom of the interim
-                stack so start the iteration again to find next one to go over */
-                interimStack = new ArrayList<>(interimStack);
-                System.out.println(interimStack);
+                }
             }
         }
     }
+
 
     public static void main(String[] args) throws Exception {
 
